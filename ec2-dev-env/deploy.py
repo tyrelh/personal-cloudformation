44002fd76fb561413ae8be00857fa9acb0e777e9
tyrelh <tyrel.hiebert@gmail.com>
2023-10-30 11:19:24 -0700

    initial deploying ec2 instance

--- ec2-dev-env/deploy.py
+++ ec2-dev-env/deploy.py
@@ -2,5 +2,8 @@ import sys
 sys.path.append("../")
 from deployutils import run_process
 
-command = "aws cloudformation --help"
+template_file = "ec2-dev-env.yml"
+stack_name = "dev-env"
+
+command = f"aws cloudformation deploy --template-file {template_file} --stack-name {stack_name} --parameter-overrides 'ProjectTag=DevEnv'"
 run_process(command)
