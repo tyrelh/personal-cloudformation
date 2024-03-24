b88bb04e2a2ef22be5c97436e16733100ab3e06b
Tyrel Delaney <tyrel.hiebert@gmail.com>
2024-03-23 20:36:36 -0700

    add role to add ssm agent

--- plausible/deploy.sh
+++ plausible/deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-aws cloudformation deploy --template-file plausible-server.yml --stack-name plausible --capabilities CAPABILITY_IAM --region ca-west-1
+aws cloudformation deploy --template-file plausible-server.yml --stack-name plausible --capabilities CAPABILITY_NAMED_IAM --region ca-west-1
